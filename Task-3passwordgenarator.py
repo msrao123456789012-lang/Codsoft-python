74d0bf8234e7fe7eb060fa03e8a98ae4adbdca2f
msrao123456789012-lang <msrao123456789012@gmail.com>
2025-08-30 22:57:53 +0530

    Create Task-3passwordgenarator.py

---
+++ Task-3passwordgenarator.py
@@ -0,0 +1,24 @@
+import random
+import string
+digits_len=int(input("enter how many digits to be involved in genarated password:"))
+letter_len=int(input("enter how many letters  to be involved in genarated password:"))
+pun_len=int(input("enter how many punctuations  to be involved in genarated password:"))
+letters=string.ascii_letters
+digits=string.digits
+punctuations=string.punctuation
+password=""
+dig_pss=""
+letter_pass=""
+pun_pass=""
+for _ in range(digits_len):
+    dig_pss+=random.choice(digits)
+for _ in range (letter_len):
+    letter_pass+=random.choice(letters)
+for _ in range(pun_len):
+    pun_pass+=random.choice(punctuations)
+password=dig_pss+letter_pass+pun_pass
+#password=''.join(random.sample(password,len(password)))
+password=list(password)
+random.shuffle(password)
+password=''.join(password)
+print("Genarated password as per your reqiuremet is:",password)
